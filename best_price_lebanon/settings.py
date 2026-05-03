27d1d513fa70ba103d249a032f84372abd01b44c
Wassim-php <omzznais@gmail.com>
2026-05-03 23:27:59 +0300

    Fixed db error

--- best_price_lebanon/settings.py
+++ best_price_lebanon/settings.py
@@ -87,7 +87,7 @@ if os.environ.get('DATABASE_URL'):
         'default': dj_database_url.config(
             conn_max_age=600,
             conn_health_checks=True,
-            ssl_require=True
+            ssl_require=False
         )
     }
 else:
